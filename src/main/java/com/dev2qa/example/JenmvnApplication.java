ccd52102bfa02669cb9cb3b9ab10a08da2507036
harrison <harrison.starwaytravel@gmail.com>
2019-04-11 14:46:33 +0800

    Initial commit

---
+++ src/main/java/com/dev2qa/example/JenmvnApplication.java
@@ -0,0 +1,13 @@
+package com.dev2qa.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenmvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenmvnApplication.class, args);
+	}
+
+}
